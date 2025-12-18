44514b09e24259c3d4db8fca5795961ce0384d7c
Amrtanshu Sharma <amrtanshusharma@Amrtanshus-MacBook-Air.local>
2025-12-18 20:02:02 +0530

    user schema and mongodb running and up

--- backend-node/src/index.js
+++ backend-node/src/index.js
@@ -1,12 +1,18 @@
+require("dotenv").config();
 const express  = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 const routes = require("./routes");
-
 const app  = express();
 
+//middleware
 app.use(cors());
 app.use(express.json());
 
+//Connect DB
+connectDB();
+
+//routes
 app.use("/api",routes);
 
 app.get("/",(req,res)=>{
