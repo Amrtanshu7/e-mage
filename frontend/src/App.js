495a849a2d7442e7eac1d416b7c5f0533d8c230e
Amrtanshu Sharma <amrtanshusharma@Amrtanshus-MacBook-Air.local>
2025-12-21 22:12:27 +0530

    Setup frontend scaffold with routing and protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import ImageUploader from "./components/ImageUploader";
+import { BrowserRouter, Routes, Route }  from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App(){
+  
   return (
-    <div style = {{padding: "20px"}}>
-      <ImageUploader/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path ="/login" element ={<Login />} />
+      <Route path ="/register" element ={<Register />} />
+      <Route path = "/" element = {<ProtectedRoute>
+                                      <Dashboard />
+                                    </ProtectedRoute>
+                                  }
+                                  />
+      </Routes>
+      </BrowserRouter>
   );
 }
 
