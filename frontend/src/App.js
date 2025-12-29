1eb877b1bacaf84b72cd19fca1a6265b2f5356b1
Amrtanshu Sharma <amrtanshusharma@Amrtanshus-MacBook-Air.local>
2025-12-29 23:49:18 +0530

    Major fix broken frontend route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import { BrowserRouter, Routes, Route }  from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-function App(){
-  
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route path = "/" element = {<ProtectedRoute>
-                                      <Dashboard />
-                                    </ProtectedRoute>
-                                  }
-      />
-      <Route path ="/login" element ={<Login />} />
-      <Route path ="/register" element ={<Register />} />
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
 
-      </Routes>
-      </BrowserRouter>
-  );
+                {/* Protected routes */}
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/" element={<Dashboard />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
