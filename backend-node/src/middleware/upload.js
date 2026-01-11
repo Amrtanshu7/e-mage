9e67e63f93d8d06c17e4b5fb84d80319e433a34d
Amrtanshu Sharma <amrtanshusharma@Amrtanshus-MacBook-Air.local>
2026-01-11 15:22:47 +0530

    fixed multer file  for s3 modifications

--- backend-node/src/middleware/upload.js
+++ backend-node/src/middleware/upload.js
@@ -1,20 +1,10 @@
-const multer  = require("multer");
-const path = require("path");
+const multer = require("multer");
 
-const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null,"uploads/original/");
-    },
-    filename:(req,file,cb) => {
-        const uniqueName = 
-            Date.now() + "-" + Math.round(Math.random() *1e9);
-            cb(null, uniqueName + path.extname(file.originalname));
-    }
-});
+const storage = multer.memoryStorage();
 
-const fileFilter = (req,file,cb) => {
+const fileFilter = (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
-        cb(null,true);
+        cb(null, true);
     } else {
         cb(new Error("Only image files are allowed"), false);
     }
